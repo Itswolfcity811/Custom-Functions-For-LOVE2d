9907933be0e32e7c6b28a1b02e9fd3cee9830dfe
Itswolfcity811 <91641161+Itswolfcity811@users.noreply.github.com>
2023-05-23 18:27:03 -0500

    Update Functions.lua

--- Functions.lua
+++ Functions.lua
@@ -3,4 +3,4 @@
 ]]
 function Color(value1, value2, value3)
     return value1 / 255, value2 / 255, value3 / 255
-end --If you are used to the standered (255, 255, 255) RGB, then this will convert it to that
+end --If you are used to the standered (0-255, 0-255, 0-255) RGB, then this will convert it to (0-1, 0-1, 0-1)
