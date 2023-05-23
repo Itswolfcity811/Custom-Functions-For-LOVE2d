1c52d91b4e4f6093006dbee91d0961aef89794ed
Itswolfcity811 <91641161+Itswolfcity811@users.noreply.github.com>
2023-05-23 18:27:29 -0500

    Update Functions.lua

--- Functions.lua
+++ Functions.lua
@@ -1,6 +1,7 @@
 --[[ Make sure you you do the following in the love.load function inside of the Main.lua file 
    require("Functions")
 ]]
+
 function Color(value1, value2, value3)
     return value1 / 255, value2 / 255, value3 / 255
 end --If you are used to the standered (0-255, 0-255, 0-255) RGB, then this will convert it to (0-1, 0-1, 0-1)
