50b00308179467068b7a7e87b89f739b22a29979
Itswolfcity811 <91641161+Itswolfcity811@users.noreply.github.com>
2023-05-23 18:30:05 -0500

    Update Functions.lua
    Signed-off-by: Itswolfcity811 <91641161+Itswolfcity811@users.noreply.github.com>

--- Functions.lua
+++ Functions.lua
@@ -4,4 +4,6 @@
 
 function Color(value1, value2, value3)
     return value1 / 255, value2 / 255, value3 / 255
-end --If you are used to the standered (0-255, 0-255, 0-255) RGB, then this will convert it to (0-1, 0-1, 0-1)
+end --[[If you are used to the standered (0-255, 0-255, 0-255) RGB, then this will convert it to (0-1, 0-1, 0-1). You will use it like:
+    love.graphics.setBackgroundColor(Color(0, 0, 0))
+]]
