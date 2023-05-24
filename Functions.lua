5c608013c996d59d2b01d1ee5e330251d862cf2c
Itswolfcity811 <91641161+Itswolfcity811@users.noreply.github.com>
2023-05-23 19:56:42 -0500

    Update Functions.lua
    Signed-off-by: Itswolfcity811 <91641161+Itswolfcity811@users.noreply.github.com>

--- Functions.lua
+++ Functions.lua
@@ -1,7 +1,9 @@
 functions = {
     Color = function (value1, value2, value3)
         return value1 / 255, value2 / 255, value3 / 255
-        --[[If you are used to the standered (0-255, 0-255, 0-255) RGB, then this will convert it to (0-1, 0-1, 0-1). You will use it like:
+        --[[If you are used to the standered (0-255, 0-255, 0-255) RGB, then this will convert it to (0-1, 0-1, 0-1). You have to import it in every file like this:
+                local functions = require("Functions")
+            You will use it like:
                 love.graphics.setBackgroundColor(Functions.Color(0, 0, 0))
             ]]
     end
