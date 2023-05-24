d9accc4f077ab524406c5a05a3c1a414eede1b16
ItsWolfCity811 <91641161+Itswolfcity811@users.noreply.github.com>
2023-05-24 17:55:30 -0500

    Update Functions.lua
    Signed-off-by: ItsWolfCity811 <91641161+Itswolfcity811@users.noreply.github.com>

--- Functions.lua
+++ Functions.lua
@@ -3,15 +3,73 @@
         local functions = require("Functions")
 ]]
 
+local love = require("love")
+
 functions = {
     Color = function (value1, value2, value3)
         return value1 / 255, value2 / 255, value3 / 255
-        --[[If you are used to the standered (0-255, 0-255, 0-255) RGB, then this will convert it to (0-1, 0-1, 0-1). You have to import it in every file like this:
-                local functions = require("Functions")
-            You will use it like:
+        --[[If you are used to the standered (0-255, 0-255, 0-255) RGB, then this will convert it to (0-1, 0-1, 0-1). You will use it like:
                 love.graphics.setBackgroundColor(functions.Color(0, 0, 0))
             ]]
-    end
+    end,
+    
+    Print = function (text, x, y)
+        love.graphics.print(text, x, y)
+        return 1
+        --[[This function is a shorter way of printing to the screen. You will use it like this:
+            functions.Print("Hello World", 500, 500)
+        ]]
+    end,
+    
+    Data = {
+        Write = function (name, data, size)
+            success, message = love.filesystem.write( name, data, size )
+            if success then
+                return
+            else
+                love.errorhandler(message)
+                return
+            end
+        end, --[[This function writes to file, I'm not to sure how it works though. I think this is how you use it:
+            functions.Data.Write(name, data, size) -- I don't know how it works
+        ]]
+        
+        NewFile = function (NameOfFile) 
+            local file = love.filesystem.newFile(NameOfFile)
+            return file
+        end, --[[This function makes a new file. This is how I think you use it:
+            functions.Data.NewFile(NameOfFile) -- But you should probaly do it like this:
+            local Files = {}
+        
+            functions.Data.NewFile(NameOfFile)
+        
+            table.append(Files, file)
+        ]]
+    },
+    
+    Graphics = {},
+    
+    Mouse = {
+        Position = function ()
+            local x = love.mouse.getX
+            local y = love.mouse.getY
+            return x, y
+        end --[[Use this as a simpler way of getting the mouse x and y. You will use it like:
+            MouseX, MouseY = functions.mouse.Position()
+        ]]
+    },
+    
+    Window = {
+        Quit = function (Confermation)
+           if not Congermation then
+                love.event.quit()
+           else
+                --Not Fineshed
+           end
+        end --[[This is how you can quit the game. Use "Confermation" as a boolean value. If you put false it will just close the window, if you put true it will ask the user for a promt input and if they say yes then it closes and if not nothing happens. You will use it like this:
+            functions.Window.Quit(True) -- You could put false though
+        ]]
+    }
 }
 
 return functions
