4d8acfc6e084026d3a67b9d6e0436b7b476d5e72
Itswolfcity811 <91641161+Itswolfcity811@users.noreply.github.com>
2023-05-23 19:54:35 -0500

    Update Functions.lua
    Signed-off-by: Itswolfcity811 <91641161+Itswolfcity811@users.noreply.github.com>

--- Functions.lua
+++ Functions.lua
@@ -1,11 +1,8 @@
---[[ Make sure you you do the following in the love.load function inside of the Main.lua file 
-   require("Functions")
-]]
 functions = {
     Color = function (value1, value2, value3)
         return value1 / 255, value2 / 255, value3 / 255
         --[[If you are used to the standered (0-255, 0-255, 0-255) RGB, then this will convert it to (0-1, 0-1, 0-1). You will use it like:
-                love.graphics.setBackgroundColor(Color(0, 0, 0))
+                love.graphics.setBackgroundColor(Functions.Color(0, 0, 0))
             ]]
     end
 }
